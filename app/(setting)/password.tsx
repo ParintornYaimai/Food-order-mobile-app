bb7a7709cfca825422dc072c9ad24d40a67895a0
ParintornYaimai <parintorn-y@rmutp.ac.th>
2025-08-28 22:37:58 +0700

    feat: add setting screen

---
+++ app/(setting)/password.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const password = () => {
+  return (
+    <View>
+      <Text>password</Text>
+    </View>
+  )
+}
+
+export default password
