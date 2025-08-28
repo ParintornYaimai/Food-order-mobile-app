6340c66b721dd0caabd3bbdd08e573af8ae5eadd
ParintornYaimai <parintorn-y@rmutp.ac.th>
2025-08-28 22:37:41 +0700

    feat: add payment screen

---
+++ app/(payment)/addCard.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const addCard = () => {
+  return (
+    <View>
+      <Text>addCard</Text>
+    </View>
+  )
+}
+
+export default addCard
